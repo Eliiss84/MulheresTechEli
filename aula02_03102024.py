54e0559426d1241d4b0b0c00be6f435715f6c090
Eliiss84 <elismorena.2013@gmail.com>
2024-10-08 18:55:38 -0300

    Add files via upload

---
+++ aula02_03102024.py
@@ -0,0 +1,65 @@
+print('string') #comando para exibir meu conteudo no terminal.
+'''
+Dentro dos tipos de arquivo em Python,
+temos os tipo texto que são conhecidos
+como STRINGS; no exemplo acima, eu utilizei
+o comando PRINT para exibir uma string no meu 
+terminal Python.
+
+'''
+"""print(123)
+print(37438,23534) #2 int
+print(2963.44) #float
+print(True) #boolean
+print(False) #boolean
+print(None) #none
+print(['string']) #listas
+print(['string']) #tupla
+print(['string';'texto']) #dicionário"""
+
+###identificação###---04/10/2424
+
+# idade=20
+# idade1=13
+#idade2=21
+# if idade1>=18:
+#    print("Você é maior de idade. Pode entrar.") 
+#else:
+#     print("Você é menor de idade. Vai estudar.")
+
+
+"""temperatura=36
+print(temperatura)
+
+
+if temperatura<37.5: #Nosso primeiro teste lógico
+    print("Você não esta com febre.")
+elif temperatura==37.5:
+    print("Você esta febril.")
+else: #última opção condicional. É requisitada quando todas as condições
+    ("Você esta com febre.")"""
+
+fruta1='jambo'
+fruta2='maçã'
+fruta3='uva'
+fruta4='maçã'
+num1=67
+num2=12
+num3=12.0
+num4=67.1
+
+print(fruta1==fruta3)
+fruta1=fruta3
+print(fruta1)
+
+print(fruta1==fruta4)
+print(fruta2==fruta4)
+print(fruta3==fruta4)
+print(num1==num2)
+print(num1!=num2)
+print(num1>num2)
+print(num1<num2)
+print(num2==num3)
+print(num1!=num4)
+
+
